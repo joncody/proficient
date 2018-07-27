521780294a27e3226bf8ae677aaeb37cbb6676f4
joncody <jonjdcody@gmail.com>
2018-07-26 17:13:40 -0700

    .

--- proficient.js
+++ proficient.js
@@ -5617,7 +5617,7 @@ module.exports = {
         "video"
     ];
 
-    // FIX
+    // FIXES
     if (ArrayBuffer.prototype.slice === undefined) {
         ArrayBuffer.prototype.slice = function (start, end) {
             var that = new Uint8Array(this);
@@ -5635,6 +5635,10 @@ module.exports = {
             }
             return result;
         };
+    }
+
+    function arrSlice(value, begin, end) {
+        return Array.prototype.slice.call(value, begin, end);
     }
 
     Number.isNaN = Number.isNaN || function (value) {
@@ -5741,10 +5745,10 @@ module.exports = {
             array = value.length() === 1
                 ? [value.raw()]
                 : value.raw();
-        } else if (isBuffer(value)) {
-            array = new Uint8Array(value);
-        } else if (isString(value) || isArray(value) || isArrayLike(value) || isTypedArray(value)) {
-            array = Array.prototype.slice.call(value);
+        } else if (isNumber(value) || isBuffer(value)) {
+            array = arrSlice(new Uint8Array(value));
+        } else if (isString(value) || isArray(value) || isArrayLike(value) || isTypedArray(value) || isBuffer(value)) {
+            array = arrSlice(value);
         } else {
             array = [value];
         }
@@ -5784,11 +5788,11 @@ module.exports = {
         });
     }
 
-    function toInt(value, base) {
+    function toInt(value, radix) {
         var int = global.parseInt(isString(value)
             ? value.replace(",", "")
-            : value, isNumber(base)
-                ? base
+            : value, isNumber(radix)
+                ? radix
                 : 10);
 
         return Number.isNaN(int)
@@ -5827,10 +5831,6 @@ module.exports = {
     }
 
     // MISC
-    function arrSlice(value) {
-        return Array.prototype.slice.call(value);
-    }
-
     function betterview(buffer, offset, length) {
         var numbersandbytes = {
             "Int8": 1,
@@ -6031,12 +6031,12 @@ module.exports = {
         if (isGG(items)) {
             items.eachRaw(func);
         } else if (isNode(items)) {
-            func.call(thisarg, items, 0, items);
+            func.call(thisarg, items, 0, thisarg);
         } else if (isArray(items) || isArrayLike(items) || isTypedArray(items) || isBuffer(items)) {
             toArray(items).forEach(func, thisarg);
         } else if (isObject(items)) {
             Object.keys(items).forEach(function (key) {
-                func.call(thisarg, items[key], key, items);
+                func.call(thisarg, items[key], key, thisarg);
             });
         }
         return thisarg;
@@ -7269,6 +7269,7 @@ module.exports = {
         cdb = null;
     }
 
+    gg.arrSlice = arrSlice;
     gg.typeOf = typeOf;
     gg.isArray = isArray;
     gg.isBoolean = isBoolean;
@@ -7294,7 +7295,6 @@ module.exports = {
     gg.toUint8 = toUint8;
     gg.toBuffer = toBuffer;
     gg.toStringFromCodes = toStringFromCodes;
-    gg.arrSlice = arrSlice;
     gg.betterview = betterview;
     gg.copy = copy;
     gg.each = each;
